3dc9e8428b103b8fd4c5d462df86b3399a7370b8
Chase Simmons <csim6890@gmail.com>
2020-10-23 17:43:15 -0500

    added input fields and css

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,61 @@
 $(document).ready(initialize);
-
+///
+//  ON READY AND LISTENERS
 function initialize() {
   console.log('init');
+  getDataFromDB();
 }
-
-function getDataFromDB() {}
+///
+//
+function getDataFromDB() {
+  $.ajax({
+    method: 'GET',
+    url: '/task',
+  })
+    .then(function (response) {
+      renderTable(response);
+    })
+    .catch(function (error) {
+      alert('Could not get data from server');
+    });
+}
+///
+//
 function sendDataToDB() {}
+///
+//
 function deleteDataFromDB() {}
+///
+//
 function updateDataFromDB() {}
-function renderTable() {}
+///
+//
+function renderTable(response) {
+  let tasks = response;
+  $('.task-list').empty();
+
+  for (let task of tasks) {
+    console.log(task);
+    if (task.is_complete) {
+      $('.task-list').append(`
+    <tr class="true">
+              <td>${task.task_name}</td>
+              <td>${task.is_complete}</td>
+              <td><button class="completeButton data-index="${task.id}>COMPLETE</button> <button class="deleteButton data-index="${task.id}>DELETE</button></td>
+            </tr>
+    `);
+    } else {
+      $('.task-list').append(`
+      <tr class="false">
+                <td>${task.task_name}</td>
+                <td>${task.is_complete}</td>
+                <td><button class="completeButton data-index="${task.id}>COMPLETE</button> <button class="deleteButton data-index="${task.id}>DELETE</button></td>
+              </tr>
+      `);
+    }
+  }
+}
+///
+//
+function clearInputs() {}
+///
