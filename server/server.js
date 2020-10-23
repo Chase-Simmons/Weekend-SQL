ec05d7927092e099f5d6f9149ff3234cc5211558
Chase Simmons <csim6890@gmail.com>
2020-10-23 15:37:42 -0500

    server.js set up complete

--- server/server.js
+++ server/server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
-const artistRouter = require('./routes/artist.router.js');
-const songRouter = require('./routes/song.router.js');
+const taskRouter = require('./routes/task.router.js');
 
-// Required for our POST requests to work
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.use('/artist', artistRouter);
-app.use('/songs', songRouter);
+app.use('/task', taskRouter);
 
 app.use(express.static('server/public'));
 
