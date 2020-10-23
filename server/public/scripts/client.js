f9935cc84e2dc3c0a2b0daef9cc9c81dc3e1305f
Chase Simmons <csim6890@gmail.com>
2020-10-23 17:00:01 -0500

    added buttons on tables, prep client functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,9 @@ $(document).ready(initialize);
 function initialize() {
   console.log('init');
 }
+
+function getDataFromDB() {}
+function sendDataToDB() {}
+function deleteDataFromDB() {}
+function updateDataFromDB() {}
+function renderTable() {}
